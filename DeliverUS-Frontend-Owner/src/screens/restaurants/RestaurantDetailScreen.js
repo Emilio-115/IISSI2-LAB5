1c7c7f0c92fc833ab6d7a0d56fcd96ada371ff06
Emilio-115 <emivazcru@alum.us.es>
2024-04-05 12:03:40 +0200

    Funciona

--- DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantDetailScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantDetailScreen.js
@@ -6,16 +6,26 @@ import ImageCard from '../../components/ImageCard'
 import TextRegular from '../../components/TextRegular'
 import TextSemiBold from '../../components/TextSemibold'
 import * as GlobalStyles from '../../styles/GlobalStyles'
+import { showMessage } from 'react-native-flash-message'
 
 export default function RestaurantDetailScreen ({ route }) {
   const [restaurant, setRestaurant] = useState({})
 
   useEffect(() => {
-    console.log('Loading restaurant details, please wait 1 second')
-    setTimeout(() => {
-      setRestaurant(getDetail(route.params.id))
-      console.log('Restaurant details loaded')
-    }, 1000)
+    async function fetchRestaurantDetails () {
+      try {
+        const restaurantDetail = await getDetail(route.params.id)
+        setRestaurant(restaurantDetail)
+      } catch (error) {
+        showMessage({
+          message: `There was an error while retrieving restaurants. ${error} `,
+          type: 'error',
+          style: GlobalStyles.flashStyle,
+          titleStyle: GlobalStyles.flashTextStyle
+        })
+      }
+    }
+    fetchRestaurantDetails()
   }, [])
 
   const renderHeader = () => {
@@ -95,5 +105,5 @@ const styles = StyleSheet.create({
     textAlign: 'right',
     marginRight: 5,
     color: GlobalStyles.brandSecondary
-  },
+  }
 })
